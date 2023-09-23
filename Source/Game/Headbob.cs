7ebdc7b5d1c272c149b7ad8d6e1d622e35c4207d
bridget 3 <40956319+koszyczek@users.noreply.github.com>
2023-09-23 11:47:58 +0200

    Add files via upload

---
+++ Source/Game/Headbob.cs
@@ -0,0 +1,131 @@
+﻿using FlaxEngine;
+using System;
+
+public class HeadBob : Script
+{
+    private Vector3 _originalPosition;
+    private float _timer = 0;
+    private int _lastPlayedSoundIndex = -1; // To prevent playing the same sound twice in a row
+    private System.Random _random = new System.Random(); // For generating random numbers
+
+    // Adjust these values to fit your needs
+    public float BobbingSpeed = 0.18f;
+    public float SprintBobbingSpeed = 0.25f; // New variable for sprinting bobbing speed
+    public float BobbingAmount = 0.2f;
+    public AudioClip[] BobSounds; 
+    public AudioClip JumpSound; // The AudioClip to play when jumping
+    public AudioClip LandingSound; // The AudioClip to play when landing
+    public AudioSource audio;// The array of AudioClips to play on each bob
+    public AudioSource LandingAudio; // The AudioSource to play the landing sound
+
+    
+
+    private AudioSource _audioSource; // The AudioSource component
+    public CharacterController _characterController; // Reference to the CharacterController
+    private bool _wasInAir = false; // To track if the player was in the air in the last frame
+
+    private Quaternion _originalRotation; // The original rotation of the camera
+
+    /// <inheritdoc />
+    public override void OnStart()
+    {
+        // Store the original position of the camera (or object) this script is attached to
+        _originalPosition = Actor.LocalPosition;
+
+        // Get or add the AudioSource component
+        _audioSource = audio;
+
+        // Get the CharacterController component
+
+        // Store the original rotation of the camera
+        _originalRotation = Actor.Orientation;
+    }
+
+    /// <inheritdoc />
+    public override void OnUpdate()
+    {
+        float waveslice = 0.0f;
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 localPosition = Actor.LocalPosition;
+
+        if (Mathf.Abs(horizontal) == 0 && Mathf.Abs(vertical) == 0)
+        {
+            _timer = 0.0f;
+        }
+        else
+        {
+            waveslice = Mathf.Sin(_timer);
+            _timer = _timer + (IsSprinting() ? SprintBobbingSpeed : BobbingSpeed) * Time.DeltaTime; // Use SprintBobbingSpeed if sprinting
+            if (_timer > Mathf.Pi * 2)
+            {
+                _timer = _timer - (Mathf.Pi * 2);
+
+                // Only play the bobbing sound if the player is grounded
+                if (_characterController.IsGrounded && BobSounds.Length > 0)
+                {
+                    int randomIndex;
+                    do
+                    {
+                        randomIndex = _random.Next(BobSounds.Length);
+                    } while (randomIndex == _lastPlayedSoundIndex); // Ensure we don't play the same sound twice
+                    _lastPlayedSoundIndex = randomIndex;
+
+                    // Set the selected sound as the source
+                    _audioSource.Clip = BobSounds[randomIndex];
+
+                    // Play the sound
+                    _audioSource.Play();
+                }
+            }
+        }
+
+        if (waveslice != 0)
+        {
+            float translateChange = waveslice * BobbingAmount;
+            float totalAxes = Mathf.Abs(horizontal) + Mathf.Abs(vertical);
+            totalAxes = Mathf.Clamp(totalAxes, 0.0f, 1.0f);
+            translateChange = totalAxes * translateChange;
+
+            // Apply the bobbing effect
+            localPosition.Y = _originalPosition.Y + translateChange;
+        }
+        else
+        {
+            localPosition.Y = _originalPosition.Y;
+        }
+
+        Actor.LocalPosition = localPosition;
+
+        // Play the landing sound if the player just landed
+        if (_wasInAir && _characterController.IsGrounded)
+        {
+            LandingAudio.Clip = LandingSound;
+            LandingAudio.Play();
+        }
+
+        // Play the jumping sound if the player is jumping
+        if (IsJumping() && _characterController.IsGrounded)
+        {
+            _audioSource.Clip = JumpSound;
+            _audioSource.Play();
+        }
+
+        // Remember if the player is in the air for the next frame
+        _wasInAir = !_characterController.IsGrounded;
+
+        // Calculate the lean angle based on the player's movement direction
+        
+    }
+
+    private bool IsSprinting()
+    {
+        return Input.GetAction("Sprint");
+    }
+
+    private bool IsJumping()
+    {
+        return Input.GetAction("Jump");
+    }
+}
---
+++ Source/Game/PlayerScript.cs
@@ -0,0 +1,201 @@
+using System;
+using FlaxEngine;
+
+public class PlayerScript : Script
+{
+    public CharacterController PlayerController;
+    public Actor CameraTarget;
+    public Camera Camera;
+
+
+    public float CameraSmoothing = 20.0f;
+
+    public bool CanJump = true;
+    public bool UseMouse = true;
+    public float JumpForce = 800;
+
+    public float Friction = 8.0f;
+    public float GroundAccelerate = 5000;
+    public float AirAccelerate = 10000;
+    public float MaxVelocityGround = 400;
+    public float MaxVelocityAir = 200;
+
+    private Vector3 _velocity;
+    private bool _jump;
+    private float _pitch;
+    private float _yaw;
+    private float _horizontal;
+    private float _vertical;
+    public float SprintSpeed = 800;
+    // Add a normal speed variable
+    public float NormalSpeed = 400;
+    // Add a FOV for sprinting
+    public float SprintFOV = 75f;
+    // Add a normal FOV
+    public float NormalFOV = 60f;
+    // Add a variable to control the speed of FOV change
+    public float FOVSpeed = 5f;
+    /// <summary>
+    /// Adds the movement and rotation to the camera (as input).
+    /// </summary>
+    /// <param name="horizontal">The horizontal input.</param>
+    /// <param name="vertical">The vertical input.</param>
+    /// <param name="pitch">The pitch rotation input.</param>
+    /// <param name="yaw">The yaw rotation input.</param>
+    public void AddMovementRotation(float horizontal, float vertical, float pitch, float yaw)
+    {
+        _pitch += pitch;
+        _yaw += yaw;
+        _horizontal += horizontal;
+        _vertical += vertical;
+    }
+
+    public override void OnUpdate()
+    {
+        if (UseMouse)
+        {
+            // Cursor
+            Screen.CursorVisible = false;
+            Screen.CursorLock = CursorLockMode.Locked;
+
+            // Mouse
+            var mouseDelta = new Float2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+            _pitch = Mathf.Clamp(_pitch + mouseDelta.Y, -88, 88);
+            _yaw += mouseDelta.X;
+        }
+
+        // Jump
+        if (CanJump && Input.GetAction("Jump"))
+            _jump = true;
+
+        if (Input.GetAction("Sprint"))
+        {
+            // Increase the max velocity to sprint speed
+            MaxVelocityGround = SprintSpeed;
+            MaxVelocityAir = SprintSpeed;
+            // Gradually change the FOV to the sprint FOV
+            Camera.FieldOfView = Mathf.Lerp(Camera.FieldOfView, SprintFOV, Time.DeltaTime * FOVSpeed);
+        }
+        else
+        {
+            // Reset the max velocity to normal speed
+            MaxVelocityGround = NormalSpeed;
+            MaxVelocityAir = NormalSpeed;
+            // Gradually change the FOV back to the normal FOV
+            Camera.FieldOfView = Mathf.Lerp(Camera.FieldOfView, NormalFOV, Time.DeltaTime * FOVSpeed);
+        }
+
+    }
+
+    private Vector3 Horizontal(Vector3 v)
+    {
+        return new Vector3(v.X, 0, v.Z);
+    }
+
+    public override void OnFixedUpdate()
+    {
+        // Update camera
+        var camTrans = Camera.Transform;
+        var camFactor = Mathf.Saturate(CameraSmoothing * Time.DeltaTime);
+        CameraTarget.LocalOrientation = Quaternion.Lerp(CameraTarget.LocalOrientation, Quaternion.Euler(_pitch, _yaw, 0), camFactor);
+        //CameraTarget.LocalOrientation = Quaternion.Euler(pitch, yaw, 0);
+        camTrans.Translation = Vector3.Lerp(camTrans.Translation, CameraTarget.Position, camFactor);
+        camTrans.Orientation = CameraTarget.Orientation;
+        Camera.Transform = camTrans;
+
+        var inputH = Input.GetAxis("Horizontal") + _horizontal;
+        var inputV = Input.GetAxis("Vertical") + _vertical;
+        _horizontal = 0;
+        _vertical = 0;
+
+        var velocity = new Vector3(inputH, 0.0f, inputV);
+        velocity.Normalize();
+        Vector3 rotation = CameraTarget.LocalEulerAngles;
+        rotation.X = 0;
+        rotation.Z = 0;
+        velocity = Vector3.Transform(velocity, Quaternion.Euler(rotation));
+
+        if (PlayerController.IsGrounded)
+        {
+            velocity = MoveGround(velocity.Normalized, Horizontal(_velocity));
+            velocity.Y = -Mathf.Abs(Physics.Gravity.Y * 0.5f);
+        }
+        else
+        {
+            velocity = MoveAir(velocity.Normalized, Horizontal(_velocity));
+            velocity.Y = _velocity.Y;
+        }
+
+        // Fix direction
+        if (velocity.Length < 0.05f)
+            velocity = Vector3.Zero;
+
+        // Jump
+        if (_jump && PlayerController.IsGrounded)
+        {
+            velocity.Y = JumpForce;
+            // Reset the vertical velocity after jumping
+            _velocity.Y = 0;
+        }
+        _jump = false;
+
+        // Apply gravity
+        velocity.Y += -Mathf.Abs(Physics.Gravity.Y * 2.5f) * Time.DeltaTime;
+
+        // Check if player is not blocked by something above head
+        if ((PlayerController.Flags & CharacterController.CollisionFlags.Above) != 0)
+        {
+            if (velocity.Y > 0)
+            {
+                // Player head hit something above, zero the gravity acceleration
+                velocity.Y = 0;
+            }
+        }
+
+        // Move
+        PlayerController.Move(velocity * Time.DeltaTime);
+        _velocity = velocity;
+    }
+
+    // accelDir: normalized direction that the player has requested to move (taking into account the movement keys and look direction)
+    // prevVelocity: The current velocity of the player, before any additional calculations
+    // accelerate: The server-defined player acceleration value
+    // maxVelocity: The server-defined maximum player velocity (this is not strictly adhered to due to strafejumping)
+    private Vector3 Accelerate(Vector3 accelDir, Vector3 prevVelocity, float accelerate, float maxVelocity)
+    {
+        float projVel = (float)Vector3.Dot(prevVelocity, accelDir); // Vector projection of Current velocity onto accelDir
+        float accelVel = accelerate * Time.DeltaTime; // Accelerated velocity in direction of movement
+
+        // If necessary, truncate the accelerated velocity so the vector projection does not exceed max velocity
+        if (projVel + accelVel > maxVelocity)
+            accelVel = maxVelocity - projVel;
+
+        return prevVelocity + accelDir * accelVel;
+    }
+
+    private Vector3 MoveGround(Vector3 accelDir, Vector3 prevVelocity)
+    {
+        // Apply Friction
+        var speed = prevVelocity.Length;
+        if (Math.Abs(speed) > 0.01f) // To avoid divide by zero errors
+        {
+            var drop = speed * Friction * Time.DeltaTime;
+            prevVelocity *= Mathf.Max(speed - drop, 0) / speed; // Scale the velocity based on friction
+        }
+
+        // GroundAccelerate and MaxVelocityGround are server-defined movement variables
+        return Accelerate(accelDir, prevVelocity, GroundAccelerate, MaxVelocityGround);
+    }
+
+    private Vector3 MoveAir(Vector3 accelDir, Vector3 prevVelocity)
+    {
+        // air_accelerate and max_velocity_air are server-defined movement variables
+        return Accelerate(accelDir, prevVelocity, AirAccelerate, MaxVelocityAir);
+    }
+
+    public override void OnDebugDraw()
+    {
+        var trans = PlayerController.Transform;
+        DebugDraw.DrawWireTube(trans.Translation, trans.Orientation * Quaternion.Euler(90, 0, 0), PlayerController.Radius, PlayerController.Height, Color.Blue);
+    }
+}
